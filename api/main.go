e7d8afe7d27c0d60d00e10b56eb920fb847bce1a
Melbin Thomas <melbin@Melbins-MacBook-Pro.local>
2025-05-25 11:11:31 +0100

    added configuration support

--- api/main.go
+++ api/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
+	"expvar"
+	"fmt"
 	"os"
 	"runtime"
 	"service/foundation/logger"
+
+	"github.com/ardanlabs/conf/v3"
 )
+
+var build = "develop"
 
 func main() {
 	var log *logger.Logger
@@ -28,6 +35,44 @@ func run(ctx context.Context, log *logger.Logger) error {
 	// GOMAXPROCS
 
 	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -------------------------------------------------------------------------
+	// Configuration
+	cfg := struct {
+		conf.Version
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "sales service",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for ouput %w", err)
+	}
+
+	log.Info(ctx, "startup", "config", out)
+
+	log.BuildInfo(ctx)
+
+	expvar.NewString("build").Set(cfg.Build)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module service
 
 go 1.24.3
+
+require github.com/ardanlabs/conf/v3 v3.7.2
