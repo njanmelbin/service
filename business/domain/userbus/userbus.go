db576102d383b1655e2dd27b1e5985496e1201e8
Melbin Thomas <njanmelbin@gmail.com>
2025-06-16 21:34:17 +0100

    added storer interface

--- business/domain/userbus/userbus.go
+++ business/domain/userbus/userbus.go
@@ -1,1 +1,27 @@
 package userbus
+
+import (
+	"context"
+	"errors"
+	"net/mail"
+	"service/business/api/order"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrNotFound              = errors.New("user not found")
+	ErrUniqueEmail           = errors.New("email is not unique")
+	ErrAuthenticationFailure = errors.New("authenticaton failed")
+)
+
+type Storer interface {
+	Create(ctx context.Context, usr User) error
+	Update(ctx context.Context, usr User) error
+	Delete(ctx context.Context, usr User) error
+	Query(ctx context.Context, filter QueryFilter, orderBy order.By, pageNumber int, rowsPerPage int) ([]User, error)
+	Count(ctx context.Context, filter QueryFilter) (int, error)
+	QueryByID(ctx context.Context, userID uuid.UUID) (User, error)
+	QueryByIDs(ctx context.Context, userID uuid.UUID) ([]User, error)
+	QueryByEmail(ctx context.Context, email mail.Address) (User, error)
+}
