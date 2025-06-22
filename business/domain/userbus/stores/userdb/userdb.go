7a939903aee0d0008f12722e1457a5a37170f3db
Melbin Thomas <njanmelbin@gmail.com>
2025-06-22 11:21:50 +0100

    userdb added

--- business/domain/userbus/stores/userdb/userdb.go
+++ business/domain/userbus/stores/userdb/userdb.go
@@ -1,1 +1,42 @@
 package userdb
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"service/business/domain/userbus"
+	"service/business/sdk/sqldb"
+	"service/foundation/logger"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// Store manages the set of APIs for user database access.
+type Store struct {
+	log *logger.Logger
+	db  *sqlx.DB
+}
+
+// NewStore constructs the api for data access.
+func NewStore(log *logger.Logger, db *sqlx.DB) *Store {
+	return &Store{
+		log: log,
+		db:  db,
+	}
+}
+
+func (s *Store) Create(ctx context.Context, usr userbus.User) error {
+	const q = `INSERT INTO users
+		(user_id, name, email, password_hash, roles, department, enabled, date_created, date_updated)
+	VALUES
+		(:user_id, :name, :email, :password_hash, :roles, :department, :enabled, :date_created, :date_updated)`
+
+	if err := sqldb.NamedExecContext(ctx, s.log, s.db, q, toDBUser(usr)); err != nil {
+		if errors.Is(err, sqldb.ErrDBDuplicatedEntry) {
+			return fmt.Errorf("namedexeccontext: %w", userbus.ErrUniqueEmail)
+		}
+		return fmt.Errorf("namedexeccontext: %w", err)
+	}
+	return nil
+
+}
