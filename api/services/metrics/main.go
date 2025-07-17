6f92ccafe7e58d8ba44a2fd416ea6cfd6b2295a7
Melbin Thomas <njanmelbin@gmail.com>
2025-07-17 21:17:34 +0100

    started metrics service

---
+++ api/services/metrics/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"os"
+	"service/foundation/logger"
+)
+
+var build = "develop"
+
+func main() {
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) { log.Info(ctx, "******* SEND ALERT ******") },
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return "00000000-0000-0000-0000-000000000000"
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "METRICS", traceIDFn, events)
+
+	// -------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "err", err)
+		os.Exit(1)
+	}
+}
+func run(ctx context.Context, log *logger.Logger) error {
+	return nil
+}
