df691fbf206f9c26f4297c9eaabe9c4e44ed5fbf
Melbin Thomas <njanmelbin@gmail.com>
2025-07-31 20:04:40 +0100

    update apitest

--- app/sdk/apitest/apitest.go
+++ app/sdk/apitest/apitest.go
@@ -1,9 +1,13 @@
 package apitest
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"service/app/sdk/auth"
 	"service/business/sdk/dbtest"
+	"testing"
 )
 
 type testOption struct {
@@ -26,4 +30,60 @@ type Test struct {
 	DB   *dbtest.Database
 	Auth *auth.Auth
 	mux  http.Handler
+}
+
+// Run performs the actual test logic based on the table data.
+func (at *Test) Run(t *testing.T, table []Table, testName string, options ...OptionFunc) {
+	to := new(testOption)
+	for _, f := range options {
+		f(to)
+	}
+
+	if to.skip {
+		t.Skipf("%v: %v", testName, to.skipMsg)
+	}
+
+	for _, tt := range table {
+		f := func(t *testing.T) {
+			r := httptest.NewRequest(tt.Method, tt.URL, nil)
+			w := httptest.NewRecorder()
+
+			if tt.Input != nil {
+				d, err := json.Marshal(tt.Input)
+				if err != nil {
+					t.Fatalf("Should be able to marshal the model : %s", err)
+				}
+
+				r = httptest.NewRequest(tt.Method, tt.URL, bytes.NewBuffer(d))
+			}
+
+			r.Header.Set("Authorization", "Bearer "+tt.Token)
+			at.mux.ServeHTTP(w, r)
+
+			if w.Code != tt.StatusCode {
+				t.Fatalf("%s: Should receive a status code of %d for the response : %d", tt.Name, tt.StatusCode, w.Code)
+			}
+
+			if tt.StatusCode == http.StatusNoContent {
+				return
+			}
+
+			if err := json.Unmarshal(w.Body.Bytes(), tt.GotResp); err != nil {
+				t.Fatalf("Should be able to unmarshal the response : %s", err)
+			}
+
+			diff := tt.CmpFunc(tt.GotResp, tt.ExpResp)
+			if diff != "" {
+				t.Log("DIFF")
+				t.Logf("%s", diff)
+				t.Log("GOT")
+				t.Logf("%#v", tt.GotResp)
+				t.Log("EXP")
+				t.Logf("%#v", tt.ExpResp)
+				t.Fatalf("Should get the expected response")
+			}
+		}
+
+		t.Run(testName+"-"+tt.Name, f)
+	}
 }
